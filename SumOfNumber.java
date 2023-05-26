0d568d48715cdcbed280a426045cefd06527c137
maddelakrishna123 <maddelakrishnakumar@gmail.com>
2023-05-26 16:57:52 +0530

    first commit

---
+++ SumOfNumber.java
@@ -0,0 +1,79 @@
+
+/*
+Addition of two numbers
+
+Input :  30 40 
+
+
+OutPut : 70
+
+
+Token : basic building blocks of java program statement
+
+
+
+          I )Identifiers : 
+		  
+		      are names given to classes, interfaces , methods and variables
+			  
+			  
+			  
+			   
+			     SumofNubers 
+ main 
+
+   num1 
+num2 
+ Rule for Identiers   
+
+   a) Identiers should contains only 
+
+             i) alplanumeric (a-z,A-Z,0-9) 
+             ii) _ (underscore and Doller $)		
+    b) should not start with digits 
+
+    c) words in identifies should not seperate with space 	
+	
+	
+	
+	
+	Naming conventions  // coding standards 
+	
+	   1) every word in  class names must start with  capital case letter 
+	   
+	   2) method and variable name should with small case lettes ,
+	   
+	      if variable and method names contains morethen one word 
+		  
+		        first word mustbe in small case  and from second word onwars should start word capital case lette 
+				
+				
+				
+	II ) Keywords : reserved words 
+class 	
+
+all keywords are written in small  case letter
+	   
+
+*/
+
+class SumOfNumber{  // class definition statments 
+
+
+public static void main(String[] args){  // method definition statment
+
+
+    int  firstnumber ;
+	int num1 ;  //  variable declaration 
+num1 = 30;  // assignment statment
+
+int num2 = 40 ;  // declaration and assignment statment
+int sum = num1 + num2; // expression
+
+System.out.println(sum);  // output statements
+
+
+}
+}
+
+// Single line comment 
